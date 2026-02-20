77b848a40c281fc2a8d8caf3596b173261fdaa2b
craazyghoost-blip <craazyghoost@gmail.com>
2026-02-20 17:56:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,24 @@
 import os
 import requests
 from fastapi import FastAPI
-from eth_account import Account
-import json
-import time
 
 app = FastAPI()
 
-PRIVATE_KEY = os.getenv("API_SECRET")
 SYMBOL = os.getenv("SYMBOL")
 SIDE = os.getenv("SIDE")
 SIZE = float(os.getenv("QUANTITY"))
 
-API_URL = "https://api.hyperliquid.xyz/exchange"
-
-account = Account.from_key(PRIVATE_KEY)
+API_URL = "https://api.hyperliquid.xyz/info"
 
 @app.get("/")
 def place_order():
-    is_buy = True if SIDE == "BUY" else False
-    
     order = {
         "type": "market",
         "symbol": SYMBOL,
-        "side": "buy" if is_buy else "sell",
+        "side": "buy" if SIDE == "BUY" else "sell",
         "size": SIZE
     }
 
-    response = requests.post(API_URL, json=order)
+    r = requests.post(API_URL, json=order)
 
-    return {"status": response.json()}
+    return {"response": r.text}
