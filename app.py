3ef7ef434806966a1980961974504b05214d64cc
craazyghoost-blip <craazyghoost@gmail.com>
2026-04-06 22:50:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = FastAPI()
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 SYMBOL = "SOL"
 POSITION_PERCENT = 0.97
-TP_PERCENT = 0.019
+TP_PERCENT = 0.003
 # ==================
 
 if not PRIVATE_KEY:
